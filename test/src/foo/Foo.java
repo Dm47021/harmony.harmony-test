5e774da55744158816272a521592bc86c20b18e2
Jean-R?my Falleri <jr.falleri@laposte.net>
2012-07-02 10:42:58 +0200

    Bar is now private. Constructor now calls bar.

---
+++ test/src/foo/Foo.java
@@ -0,0 +1,15 @@
+package foo;
+
+public class Foo extends Bar {
+	
+	private Foo() {
+		System.out.println("Foo!");
+		id = 0;
+		bar();
+	}
+
+	private void bar() {
+		System.out.println("Bar!");
+	}
+	
+}
